7b1822a31085ba81d3ffc4aba0e94cc569342c36
RobinTeotia <62256462+RobinTeotia@users.noreply.github.com>
2022-06-18 23:18:29 -0700

    Update server.py

--- server.py
+++ server.py
@@ -1,19 +1,8 @@
-import os
-import http.server
-import socketserver
+from flask import Flask
 
-from http import HTTPStatus
+app = Flask(__name__)
 
 
-class Handler(http.server.SimpleHTTPRequestHandler):
-    def do_GET(self):
-        self.send_response(HTTPStatus.OK)
-        self.end_headers()
-        msg = 'Hello! It is really giving pain now %s' % (self.path)
-        self.wfile.write(msg.encode())
-
-
-port = int(os.getenv('PORT', 80))
-print('Listening on port %s' % (port))
-httpd = socketserver.TCPServer(('', port), Handler)
-httpd.serve_forever()
+@app.route('/')
+def hello():
+    return 'Hello, World!'
